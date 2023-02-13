c08dd683e35b95828557f74fa46a077d1893f1e1
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-02-13 09:21:36 +0100

    Changed folder structure, create algorithm file, study end date is DAP specific

--- p_steps/codelists/create_conceptsets.R
+++ p_steps/codelists/create_conceptsets.R
@@ -2,20 +2,20 @@
 
 #####GDM####
 #GDM diagnoses
-codelist_gdm<-fread(paste0(pre_dir,"gestational_diabetes_codelist.csv"), colClasses = "character")
+codelist_gdm<-fread(paste0(pre_dir,"codelists/gestational_diabetes_codelist.csv"), colClasses = "character")
 #select only necessary columns
-codelist_gdm<-codelist_gdm[,c("event_definition", "coding_system", "code","tags")]
+codelist_gdm<-codelist_gdm[,c("event_abbreviation", "coding_system", "code","tags")]
 codelist_gdm<-codelist_gdm[,coding_system:=gsub("/","",coding_system)]
 #Create variable code_no_dot by removing dot from all codes
 codelist_gdm[,code_no_dot:=gsub("\\.","",codelist_gdm[,code])]
 vocabularies_list_gdm<-codelist_gdm[!duplicated(coding_system), coding_system]
-conditions_gdm<-vector(mode="list", length=length(unique(na.omit(codelist_gdm[,event_definition]))))
-names(conditions_gdm)<-unique(na.omit(codelist_gdm[,event_definition]))
+conditions_gdm<-vector(mode="list", length=length(unique(na.omit(codelist_gdm[,event_abbreviation]))))
+names(conditions_gdm)<-unique(na.omit(codelist_gdm[,event_abbreviation]))
 for (i in 1:length(conditions_gdm)){
   vocabularies<-vector(mode="list", length=length(unique(na.omit(codelist_gdm[,coding_system]))))
   names(vocabularies)<-unique(na.omit(codelist_gdm[,coding_system]))
   for (j in 1:length(vocabularies)){
-    vocabularies[[j]]<-codelist_gdm[event_definition==names(conditions_gdm)[i] & coding_system==names(vocabularies)[j], code_no_dot]
+    vocabularies[[j]]<-codelist_gdm[event_abbreviation==names(conditions_gdm)[i] & coding_system==names(vocabularies)[j], code_no_dot]
   }
   conditions_gdm[[i]]<-list.append(conditions_gdm[[i]],vocabularies)
   rm(vocabularies)
@@ -26,20 +26,20 @@ conditions_gdm<-lapply(conditions_gdm, function(x) Filter(length, x))
 
 ####PE####
 #PE diagnoses
-codelist_pe<-fread(paste0(pre_dir,"pre_eclampsia_codelist.csv"), colClasses = "character")
+codelist_pe<-fread(paste0(pre_dir,"codelists/pre_eclampsia_codelist.csv"), colClasses = "character")
 #select only necessary columns
-codelist_pe<-codelist_pe[,c("event_definition", "coding_system", "code","tags")]
+codelist_pe<-codelist_pe[,c("event_abbreviation", "coding_system", "code","tags")]
 codelist_pe<-codelist_pe[,coding_system:=gsub("/","",coding_system)]
 #Create variable code_no_dot by removing dot from all codes
 codelist_pe[,code_no_dot:=gsub("\\.","",codelist_pe[,code])]
 vocabularies_list_pe<-codelist_pe[!duplicated(coding_system), coding_system]
-conditions_pe<-vector(mode="list", length=length(unique(na.omit(codelist_pe[,event_definition]))))
-names(conditions_pe)<-unique(na.omit(codelist_pe[,event_definition]))
+conditions_pe<-vector(mode="list", length=length(unique(na.omit(codelist_pe[,event_abbreviation]))))
+names(conditions_pe)<-unique(na.omit(codelist_pe[,event_abbreviation]))
 for (i in 1:length(conditions_pe)){
   vocabularies<-vector(mode="list", length=length(unique(na.omit(codelist_pe[,coding_system]))))
   names(vocabularies)<-unique(na.omit(codelist_pe[,coding_system]))
   for (j in 1:length(vocabularies)){
-    vocabularies[[j]]<-codelist_pe[event_definition==names(conditions_pe)[i] & coding_system==names(vocabularies)[j], code_no_dot]
+    vocabularies[[j]]<-codelist_pe[event_abbreviation==names(conditions_pe)[i] & coding_system==names(vocabularies)[j], code_no_dot]
   }
   conditions_pe[[i]]<-list.append(conditions_pe[[i]],vocabularies)
   rm(vocabularies)
