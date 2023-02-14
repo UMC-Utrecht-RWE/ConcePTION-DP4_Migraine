86562c49b28143fe220ad7987007a6c36ee38cc4
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-02-14 14:02:00 +0100

    Add additional concepts file, change diagnoses script accordingly

--- p_steps/parameters/parameters_metadata.R
+++ p_steps/parameters/parameters_metadata.R
@@ -2,3 +2,10 @@
 tables_file<-list.files(paste0(projectFolder,"/p_parameters/"), "table_search")
 tables_search<-read_excel(paste0(projectFolder,"/p_parameters/",tables_file),col_types = "text")
 tables_search<-as.data.table(tables_search)
+
+additional_concepts_file<-list.files(paste0(projectFolder,"/p_parameters/"), "additional_concepts")
+additional_concepts<-read_excel(paste0(projectFolder,"/p_parameters/",additional_concepts_file),col_types = "text")
+additional_concepts<-as.data.table(additional_concepts)
+
+
+
