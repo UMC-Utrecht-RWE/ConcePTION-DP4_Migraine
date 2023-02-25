c86b838f9990b1902229ec76d5a7d3c6413b8200
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-02-25 18:18:11 +0100

    remove records before min preg date, and after max preg date

--- p_steps/Step_00_create_clean_pregnancy_D3.R
+++ p_steps/Step_00_create_clean_pregnancy_D3.R
@@ -13,7 +13,10 @@ preg_file<-list.files(paste0(thisdir,"/g_output/"), "pregnancy_final")
 #decluter environment and set up the 99_path again
 
 pregnancy_D3<-get(load(paste0(thisdir,"/g_output/", preg_file)))
+pregnancy_D3[,pregnancy_start_date:=as.IDate(pregnancy_start_date)]
+pregnancy_D3[,pregnancy_end_date:=as.IDate(pregnancy_end_date)]
 
+original_rows<-pregnancy_D3[,.N]
 #check for issues
 issues_preg_alg_sex<-pregnancy_D3[sex_at_instance_creation!="F",.N]
 pregnancy_D3<-pregnancy_D3[sex_at_instance_creation=="F"]
@@ -29,16 +32,54 @@ pregnancy_D3[,diff:=pregnancy_end_date- pregnancy_start_date]
 issues_preg_alg_ga<-pregnancy_D3[diff>=301,.N]
 pregnancy_D3<-pregnancy_D3[diff<301]
 
+#Remove all records with date before min preg date
+#Load CDM Source table
+if(length(list.files(path_dir,"^CDM_SOURCE"))>1){
+  stop("More than one CDM SOURCE table is present in the working directory. Fix error and run the script again.")
+}
+CDM_SOURCE<-fread(paste0(path_dir, list.files(path_dir,"^CDM_SOURCE")))
 
-issues<-data.table(Indicator=c("Records with sex other than female", 
+
+if(CDM_SOURCE[,data_access_provider_name] %in% c("FISABIO")){
+  min_preg_date<-as.IDate("20100401","%Y%m%d")
+}else{
+  min_preg_date<-as.IDate("20050401","%Y%m%d")
+}
+pregnancy_D3[,min_preg_date:=min_preg_date]
+pregnancy_D3[,dif:=min_preg_date - pregnancy_start_date]
+before_min_date<-pregnancy_D3[dif>0,.N]
+pregnancy_D3<-pregnancy_D3[dif<=0]
+pregnancy_D3[,min_preg_date:=NULL][,dif:=NULL]
+
+if(CDM_SOURCE[,data_access_provider_name] %in% c("THL", "GePaRD")){
+  max_preg_date<-as.IDate("20180301","%Y%m%d")
+}else{
+  max_preg_date<-as.IDate("20190301","%Y%m%d")
+}
+
+pregnancy_D3[,max_preg_date:=max_preg_date]
+pregnancy_D3[,dif:=max_preg_date - pregnancy_start_date]
+after_max_date<-pregnancy_D3[dif<0,.N]
+pregnancy_D3<-pregnancy_D3[dif>=0]
+pregnancy_D3[,max_preg_date:=NULL][,dif:=NULL]
+
+issues<-data.table(Indicator=c("Number of original records from the pregnancy algorithm",
+                               "Records with sex other than female", 
                                "Records with missing start or end date of pregnancy or both",
                                "Records with missing person id",
                                "Records with missing pregnancy id",
-                               "Records where gestational age loger than 43 weeks(301 days)"), 
-                               no_records=c(issues_preg_alg_sex,
+                               "Records where gestational age loger than 43 weeks(301 days)",
+                               paste0("Records with start date before: ", min_preg_date),
+                               paste0("Records with end date after: ", max_preg_date),
+                               "Records left after removing all issues above"), 
+                               no_records=c(original_rows,
+                                            issues_preg_alg_sex,
                                             issues_preg_alg_date,
                                             issues_preg_alg_pid,
-                                            issues_preg_alg_prid,issues_preg_alg_ga))
+                                            issues_preg_alg_prid,issues_preg_alg_ga,
+                                            before_min_date,
+                                            after_max_date,
+                                            pregnancy_D3[,.N]))
 rm(issues_preg_alg_sex,issues_preg_alg_date,issues_preg_alg_pid,issues_preg_alg_prid,issues_preg_alg_ga)
 
 if(issues[no_records>0,.N]>0){
