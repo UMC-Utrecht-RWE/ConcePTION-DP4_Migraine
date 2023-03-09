7258fdd1c74e79923d80b2d2986f66906313c8c2
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-03-09 17:55:55 +0100

    Update torun.R

--- torun.R
+++ torun.R
@@ -11,17 +11,17 @@ setwd(projectFolder)
 
 #Run pregnancy algorithm and save results in g_intermediate/pregnancy_algorithm
 system.time(source(paste0(pre_dir,"Step_00_create_clean_pregnancy_D3.R")))
-system.time(render(paste0(pre_dir,"/Report_1_pregnancy_algorithm_flowchart.Rmd"), output_dir = paste0(output_dir,"Pregnancy algorithm/"), output_file = paste0(format(Sys.Date(), "%Y"),format(Sys.Date(), "%m"),format(Sys.Date(), "%d"),"_",data_access_provider_name,"_", "Pregnancy_algorithm_flowchart.html"))) 
-
-#Run diagnostic filtering script
-source(paste0(pre_dir,"Step_01_a_diagnoses_clean_up.R"))
-
-#Run medicines filtering script
-source(paste0(pre_dir,"Step_01_b_medicines_cleanup.R"))
+system.time(render(paste0(pre_dir,"/Report_0_pregnancy_algorithm_flowchart.Rmd"), output_dir = output_dir, output_file = paste0(format(Sys.Date(), "%Y"),format(Sys.Date(), "%m"),format(Sys.Date(), "%d"),"_",data_access_provider_name,"_", "Report_0_Pregnancy_algorithm_flowchart.html"))) 
 
 #Create study population(persons/observation_periods/pregnancies)
-source(paste0(pre_dir,"Step_02_create_study_population.R"))
-file.choose()
+source(paste0(pre_dir,"Step_01_create_study_population.R"))
+
+#Run diagnostic filtering script
+source(paste0(pre_dir,"Step_02_a_diagnoses_clean_up.R"))
+
+#Run medicines filtering script
+source(paste0(pre_dir,"Step_02_b_medicines_cleanup.R"))
+
 
 #Run GDM algorithm
 
