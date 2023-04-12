56f1679f6d155bb8d0e3e23e25ec19991b4ddb38
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-04-13 00:53:05 +0200

    Create clean_folders.R

---
+++ p_steps/clean_folders.R
@@ -0,0 +1,21 @@
+#Clean folders
+unlink(paste0(projectFolder,"/g_output"), recursive = T)#delete folder
+dir.create(paste0(projectFolder, "/g_output"))
+output_dir<-paste0(projectFolder,"/g_output/")
+dir.create(paste(output_dir, "Drug utilization", sep=""))
+dir.create(paste(output_dir, "Migraine algorithm", sep=""))
+dir.create(paste(output_dir, "PE and GDM algorithm", sep=""))
+dir.create(paste(output_dir, "Pregnancy algorithm", sep=""))
+dir.create(paste(output_dir, "Pregnancy study population", sep=""))
+dir.create(paste(output_dir, "Safety", sep=""))
+dir.create(paste(output_dir, "Time log", sep=""))
+
+unlink(paste0(projectFolder,"/g_intermediate"), recursive = T)#delete folder
+dir.create(paste0(projectFolder, "/g_intermediate"))
+g_intermediate<-paste0(projectFolder,"/g_intermediate/")
+dir.create(paste(g_intermediate, "gdm_algorithm", sep=""))
+dir.create(paste(g_intermediate, "migraine_algorithm", sep=""))
+dir.create(paste(g_intermediate, "pe_algorithm", sep=""))
+dir.create(paste(g_intermediate, "pregnancy_algorithm", sep=""))
+dir.create(paste(g_intermediate, "pregnancy_d3", sep=""))
+dir.create(paste(g_intermediate, "tmp", sep=""))
