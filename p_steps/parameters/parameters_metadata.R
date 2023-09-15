d03c95ce0d878555b1aed09550f30d3e3889d375
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-09-15 17:33:22 +0200

    Update parameters_metadata.R

--- p_steps/parameters/parameters_metadata.R
+++ p_steps/parameters/parameters_metadata.R
@@ -20,7 +20,7 @@ source(paste0(pre_dir,"parameters/template_error_check.R"))
 ####GDM####
 codesheet_medicines_gdm<-additional_concepts[StudyVar=="GDM_medicines" & type=="codesheet"]
 codesheet_diagnoses_gdm<-additional_concepts[StudyVar=="GDM_diagnoses" & type=="codesheet"]
-codesheet_diagnoses_gdm_cat<-additional_concepts[StudyVar=="GDM_diagnoses_cat" & type=="not_fixed"]
+codesheet_diagnoses_gdm_cat<-additional_concepts[StudyVar=="GDM_cat_diagnoses" & type=="codesheet"]
 codesheet_procedures_gdm<-additional_concepts[StudyVar=="GDM_procedures" & type=="codesheet"]
 not_fixed_gdm_ogtt_all<-additional_concepts[StudyVar=="GDM_OGTT_ALL" & type=="not_fixed"]
 not_fixed_gdm_ogtt_yes<-additional_concepts[StudyVar=="GDM_OGTT_YES" & type=="not_fixed"]
@@ -28,14 +28,14 @@ not_fixed_gdm_checkbox<-additional_concepts[StudyVar=="GDM_checkbox" & type=="no
 
 ####PE####
 codesheet_diagnoses_pe<-additional_concepts[StudyVar=="PE_diagnoses" & type=="codesheet"]
-codesheet_diagnoses_pe_cat<-additional_concepts[StudyVar=="PE_diagnoses_cat" & type=="not_fixed"]
+codesheet_diagnoses_pe_cat<-additional_concepts[StudyVar=="PE_cat_diagnoses" & type=="codesheet"]
 #codesheet_medicines_pe<-additional_concepts[StudyVar=="PE_medicines" & type=="codesheet"]
 #codesheet_procedures_pe<-additional_concepts[StudyVar=="PE_procedures" & type=="codesheet"]
 not_fixed_pe_checkbox<-additional_concepts[StudyVar=="PE_checkbox" & type=="not_fixed"]
 
 ####Migraine####
 codesheet_medicines_migraine<-additional_concepts[StudyVar=="Migraine_medicines" & type=="codesheet"]
-codesheet_diagnoses_migraine_cat<-additional_concepts[StudyVar=="Migraine_diagnoses_cat" & type=="not_fixed"]
+codesheet_diagnoses_migraine_cat<-additional_concepts[StudyVar=="Migraine_cat_diagnoses" & type=="codesheet"]
 codesheet_diagnoses_migraine<-additional_concepts[StudyVar=="Migraine_diagnoses" & type=="codesheet"]
 codesheet_procedures_migraine<-additional_concepts[StudyVar=="Migraine_procedures" & type=="codesheet"]
 
