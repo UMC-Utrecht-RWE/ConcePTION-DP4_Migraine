c7cba502bdbdfca271cf975eb0989fe01923b476
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2023-09-20 13:47:04 +0200

    Update parameters_metadata.R

--- p_steps/parameters/parameters_metadata.R
+++ p_steps/parameters/parameters_metadata.R
@@ -39,96 +39,96 @@ codesheet_diagnoses_migraine_cat<-additional_concepts[StudyVar=="Migraine_cat_di
 codesheet_diagnoses_migraine<-additional_concepts[StudyVar=="Migraine_diagnoses" & type=="codesheet"]
 codesheet_procedures_migraine<-additional_concepts[StudyVar=="Migraine_procedures" & type=="codesheet"]
 
-####Diagnoses category####
-gdm_diag_cat<-codesheet_diagnoses_gdm_cat[,c("table", "event_abbreviation","val_1","val_2","val_3","val_4","val_5","col_1","col_2","col_3","col_4","col_5","date_column","keep")]
-gdm_diag_cat[,index:=1:gdm_diag_cat[,.N]]
-if(gdm_diag_cat[,.N]>0){
-diag_cat_gdm<-list()
-ind<-1
-for(diag_ind in 1:gdm_diag_cat[,.N]){
-  if(gdm_diag_cat[diag_ind,][!is.na(val_1) & !is.na(val_2),.N]>0){
-  col_1_name<-gdm_diag_cat[diag_ind,col_1]
-  val_1<-gdm_diag_cat[diag_ind,val_1]
-  col_names<-gdm_diag_cat[diag_ind,col_2]
-  values<-c(gdm_diag_cat[diag_ind,val_2], gdm_diag_cat[diag_ind,val_3], gdm_diag_cat[diag_ind,val_4], gdm_diag_cat[diag_ind,val_5])
-  values<-values[!is.na(values)]
-  values_df<-data.table(col_names=values)
-  setnames(values_df,"col_names",col_names)
-  data<-data.table(table=gdm_diag_cat[diag_ind,table],event_abbreviation=gdm_diag_cat[diag_ind,event_abbreviation],col_1=val_1, values_df,keep=gdm_diag_cat[diag_ind,keep],index=gdm_diag_cat[diag_ind,index],date_column=gdm_diag_cat[diag_ind,date_column])
-  setnames(data,"col_1",col_1_name)
-  } else{
-    col_1_name<-gdm_diag_cat[diag_ind,col_1]
-    val_1<-gdm_diag_cat[diag_ind,val_1]
-    data<-data.table(table=gdm_diag_cat[diag_ind,table],event_abbreviation=gdm_diag_cat[diag_ind,event_abbreviation],col_1=val_1,index=gdm_diag_cat[diag_ind,index],date_column=gdm_diag_cat[diag_ind,date_column])
-    setnames(data,"col_1",col_1_name)
-}
-  diag_cat_gdm[[ind]]<-data
-  rm(data)
-  
-  ind<-ind+1
-}
-diag_cat_gdm<-rbindlist(diag_cat_gdm,fill=T)
-}else{diag_cat_gdm<-NULL}
-
-pe_diag_cat<-codesheet_diagnoses_pe_cat[,c("table", "event_abbreviation","val_1","val_2","val_3","val_4","val_5","col_1","col_2","col_3","col_4","col_5","date_column","keep")]
-pe_diag_cat[,index:=1:pe_diag_cat[,.N]]
-if(pe_diag_cat[,.N]>0){
-diag_cat_pe<-list()
-ind<-1
-for(diag_ind in 1:pe_diag_cat[,.N]){
-  if(pe_diag_cat[diag_ind,][!is.na(val_1) & !is.na(val_2),.N]>0){
-    col_1_name<-pe_diag_cat[diag_ind,col_1]
-    val_1<-pe_diag_cat[diag_ind,val_1]
-    col_names<-pe_diag_cat[diag_ind,col_2]
-    values<-c(pe_diag_cat[diag_ind,val_2], pe_diag_cat[diag_ind,val_3], pe_diag_cat[diag_ind,val_4], pe_diag_cat[diag_ind,val_5])
-    values<-values[!is.na(values)]
-    values_df<-data.table(col_names=values)
-    setnames(values_df,"col_names",col_names)
-    data<-data.table(table=pe_diag_cat[diag_ind,table],event_abbreviation=pe_diag_cat[diag_ind,event_abbreviation],col_1=val_1, values_df,keep=pe_diag_cat[diag_ind,keep],index=pe_diag_cat[diag_ind,index],date_column=pe_diag_cat[diag_ind,date_column])
-    setnames(data,"col_1",col_1_name)
-  } else{
-    col_1_name<-pe_diag_cat[diag_ind,col_1]
-    val_1<-pe_diag_cat[diag_ind,val_1]
-    data<-data.table(table=pe_diag_cat[diag_ind,table],event_abbreviation=pe_diag_cat[diag_ind,event_abbreviation],col_1=val_1,keep=pe_diag_cat[diag_ind,keep],index=pe_diag_cat[diag_ind,index],date_column=pe_diag_cat[diag_ind,date_column])
-    setnames(data,"col_1",col_1_name)
-  }
-  diag_cat_pe[[ind]]<-data
-  rm(data)
-  
-  ind<-ind+1
-}
-diag_cat_pe<-rbindlist(diag_cat_pe,fill=T)
-}else{diag_cat_pe<-NULL}
-
-migraine_diag_cat<-codesheet_diagnoses_migraine_cat[,c("table", "event_abbreviation","val_1","val_2","val_3","val_4","val_5","col_1","col_2","col_3","col_4","col_5","date_column","keep")]
-migraine_diag_cat[,index:=1:migraine_diag_cat[,.N]]
-if(migraine_diag_cat[,.N]>0){
-  diag_cat_migraine<-list()
-  ind<-1
-  for(diag_ind in 1:migraine_diag_cat[,.N]){
-    if(migraine_diag_cat[diag_ind,][!is.na(val_1) & !is.na(val_2),.N]>0){
-      col_1_name<-migraine_diag_cat[diag_ind,col_1]
-      val_1<-migraine_diag_cat[diag_ind,val_1]
-      col_names<-migraine_diag_cat[diag_ind,col_2]
-      values<-c(migraine_diag_cat[diag_ind,val_2], migraine_diag_cat[diag_ind,val_3], migraine_diag_cat[diag_ind,val_4], migraine_diag_cat[diag_ind,val_5])
-      values<-values[!is.na(values)]
-      values_df<-data.table(col_names=values)
-      setnames(values_df,"col_names",col_names)
-      data<-data.table(table=migraine_diag_cat[diag_ind,table],event_abbreviation=migraine_diag_cat[diag_ind,event_abbreviation],col_1=val_1, values_df,keep=migraine_diag_cat[diag_ind,keep],index=migraine_diag_cat[diag_ind,index],date_column=migraine_diag_cat[diag_ind,date_column])
-      setnames(data,"col_1",col_1_name)
-    } else{
-      col_1_name<-migraine_diag_cat[diag_ind,col_1]
-      val_1<-migraine_diag_cat[diag_ind,val_1]
-      data<-data.table(table=migraine_diag_cat[diag_ind,table],event_abbreviation=migraine_diag_cat[diag_ind,event_abbreviation],col_1=val_1,keep=migraine_diag_cat[diag_ind,keep],index=migraine_diag_cat[diag_ind,index],date_column=migraine_diag_cat[diag_ind,date_column])
-      setnames(data,"col_1",col_1_name)
-    }
-    diag_cat_migraine[[ind]]<-data
-    rm(data)
-    
-    ind<-ind+1
-  }
-  diag_cat_migraine<-rbindlist(diag_cat_migraine,fill=T)
-}else{diag_cat_migraine<-NULL}
+####Not needed: Diagnoses category####
+# gdm_diag_cat<-codesheet_diagnoses_gdm_cat[,c("table", "event_abbreviation","val_1","val_2","val_3","val_4","val_5","col_1","col_2","col_3","col_4","col_5","date_column","keep")]
+# gdm_diag_cat[,index:=1:gdm_diag_cat[,.N]]
+# if(gdm_diag_cat[,.N]>0){
+# diag_cat_gdm<-list()
+# ind<-1
+# for(diag_ind in 1:gdm_diag_cat[,.N]){
+#   if(gdm_diag_cat[diag_ind,][!is.na(val_1) & !is.na(val_2),.N]>0){
+#   col_1_name<-gdm_diag_cat[diag_ind,col_1]
+#   val_1<-gdm_diag_cat[diag_ind,val_1]
+#   col_names<-gdm_diag_cat[diag_ind,col_2]
+#   values<-c(gdm_diag_cat[diag_ind,val_2], gdm_diag_cat[diag_ind,val_3], gdm_diag_cat[diag_ind,val_4], gdm_diag_cat[diag_ind,val_5])
+#   values<-values[!is.na(values)]
+#   values_df<-data.table(col_names=values)
+#   setnames(values_df,"col_names",col_names)
+#   data<-data.table(table=gdm_diag_cat[diag_ind,table],event_abbreviation=gdm_diag_cat[diag_ind,event_abbreviation],col_1=val_1, values_df,keep=gdm_diag_cat[diag_ind,keep],index=gdm_diag_cat[diag_ind,index],date_column=gdm_diag_cat[diag_ind,date_column])
+#   setnames(data,"col_1",col_1_name)
+#   } else{
+#     col_1_name<-gdm_diag_cat[diag_ind,col_1]
+#     val_1<-gdm_diag_cat[diag_ind,val_1]
+#     data<-data.table(table=gdm_diag_cat[diag_ind,table],event_abbreviation=gdm_diag_cat[diag_ind,event_abbreviation],col_1=val_1,index=gdm_diag_cat[diag_ind,index],date_column=gdm_diag_cat[diag_ind,date_column])
+#     setnames(data,"col_1",col_1_name)
+# }
+#   diag_cat_gdm[[ind]]<-data
+#   rm(data)
+#   
+#   ind<-ind+1
+# }
+# diag_cat_gdm<-rbindlist(diag_cat_gdm,fill=T)
+# }else{diag_cat_gdm<-NULL}
+# 
+# pe_diag_cat<-codesheet_diagnoses_pe_cat[,c("table", "event_abbreviation","val_1","val_2","val_3","val_4","val_5","col_1","col_2","col_3","col_4","col_5","date_column","keep")]
+# pe_diag_cat[,index:=1:pe_diag_cat[,.N]]
+# if(pe_diag_cat[,.N]>0){
+# diag_cat_pe<-list()
+# ind<-1
+# for(diag_ind in 1:pe_diag_cat[,.N]){
+#   if(pe_diag_cat[diag_ind,][!is.na(val_1) & !is.na(val_2),.N]>0){
+#     col_1_name<-pe_diag_cat[diag_ind,col_1]
+#     val_1<-pe_diag_cat[diag_ind,val_1]
+#     col_names<-pe_diag_cat[diag_ind,col_2]
+#     values<-c(pe_diag_cat[diag_ind,val_2], pe_diag_cat[diag_ind,val_3], pe_diag_cat[diag_ind,val_4], pe_diag_cat[diag_ind,val_5])
+#     values<-values[!is.na(values)]
+#     values_df<-data.table(col_names=values)
+#     setnames(values_df,"col_names",col_names)
+#     data<-data.table(table=pe_diag_cat[diag_ind,table],event_abbreviation=pe_diag_cat[diag_ind,event_abbreviation],col_1=val_1, values_df,keep=pe_diag_cat[diag_ind,keep],index=pe_diag_cat[diag_ind,index],date_column=pe_diag_cat[diag_ind,date_column])
+#     setnames(data,"col_1",col_1_name)
+#   } else{
+#     col_1_name<-pe_diag_cat[diag_ind,col_1]
+#     val_1<-pe_diag_cat[diag_ind,val_1]
+#     data<-data.table(table=pe_diag_cat[diag_ind,table],event_abbreviation=pe_diag_cat[diag_ind,event_abbreviation],col_1=val_1,keep=pe_diag_cat[diag_ind,keep],index=pe_diag_cat[diag_ind,index],date_column=pe_diag_cat[diag_ind,date_column])
+#     setnames(data,"col_1",col_1_name)
+#   }
+#   diag_cat_pe[[ind]]<-data
+#   rm(data)
+#   
+#   ind<-ind+1
+# }
+# diag_cat_pe<-rbindlist(diag_cat_pe,fill=T)
+# }else{diag_cat_pe<-NULL}
+# 
+# migraine_diag_cat<-codesheet_diagnoses_migraine_cat[,c("table", "event_abbreviation","val_1","val_2","val_3","val_4","val_5","col_1","col_2","col_3","col_4","col_5","date_column","keep")]
+# migraine_diag_cat[,index:=1:migraine_diag_cat[,.N]]
+# if(migraine_diag_cat[,.N]>0){
+#   diag_cat_migraine<-list()
+#   ind<-1
+#   for(diag_ind in 1:migraine_diag_cat[,.N]){
+#     if(migraine_diag_cat[diag_ind,][!is.na(val_1) & !is.na(val_2),.N]>0){
+#       col_1_name<-migraine_diag_cat[diag_ind,col_1]
+#       val_1<-migraine_diag_cat[diag_ind,val_1]
+#       col_names<-migraine_diag_cat[diag_ind,col_2]
+#       values<-c(migraine_diag_cat[diag_ind,val_2], migraine_diag_cat[diag_ind,val_3], migraine_diag_cat[diag_ind,val_4], migraine_diag_cat[diag_ind,val_5])
+#       values<-values[!is.na(values)]
+#       values_df<-data.table(col_names=values)
+#       setnames(values_df,"col_names",col_names)
+#       data<-data.table(table=migraine_diag_cat[diag_ind,table],event_abbreviation=migraine_diag_cat[diag_ind,event_abbreviation],col_1=val_1, values_df,keep=migraine_diag_cat[diag_ind,keep],index=migraine_diag_cat[diag_ind,index],date_column=migraine_diag_cat[diag_ind,date_column])
+#       setnames(data,"col_1",col_1_name)
+#     } else{
+#       col_1_name<-migraine_diag_cat[diag_ind,col_1]
+#       val_1<-migraine_diag_cat[diag_ind,val_1]
+#       data<-data.table(table=migraine_diag_cat[diag_ind,table],event_abbreviation=migraine_diag_cat[diag_ind,event_abbreviation],col_1=val_1,keep=migraine_diag_cat[diag_ind,keep],index=migraine_diag_cat[diag_ind,index],date_column=migraine_diag_cat[diag_ind,date_column])
+#       setnames(data,"col_1",col_1_name)
+#     }
+#     diag_cat_migraine[[ind]]<-data
+#     rm(data)
+#     
+#     ind<-ind+1
+#   }
+#   diag_cat_migraine<-rbindlist(diag_cat_migraine,fill=T)
+# }else{diag_cat_migraine<-NULL}
 
 #Gather information about checkbox
 #GDM
