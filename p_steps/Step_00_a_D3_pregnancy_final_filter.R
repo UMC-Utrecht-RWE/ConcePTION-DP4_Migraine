81fc144d6fb3235768b320a1b9aa954c635f8c51
Constanza <117450641+ConstanzaUtrecht@users.noreply.github.com>
2024-02-28 15:23:42 +0100

    Update Step_00_a_D3_pregnancy_final_filter.R
    replace data_source_name to data_access_provider_name to align with the rest of the script.

--- p_steps/Step_00_a_D3_pregnancy_final_filter.R
+++ p_steps/Step_00_a_D3_pregnancy_final_filter.R
@@ -1,7 +1,7 @@
 library(data.table)
 
 # UOSL: green and yellow from PROMPT and green yellow and blue from CONCEPTSET
-if(data_source_name=="UOSL"){
+if(data_access_provider_name=="UOSL"){
 D3_filtered <- rbind(pregnancy_D3[(meaning_of_principal_record == "birth_registry_mother" & imputed_end_of_pregnancy == 0)],
                      pregnancy_D3[!meaning_of_principal_record %in% c("birth_registry_mother", "PERSON_RELATIONSHIP")][highest_quality=="2_yellow"][type_of_pregnancy_end == "SA" | type_of_pregnancy_end == "T"])
 rm(pregnancy_D3)
@@ -11,7 +11,7 @@ rm(D3_filtered)
 
 
 # SAIL: PROMPT & EUROCAT only quality green and yellow
-if(data_source_name=="USWAN"){
+if(data_access_provider_name=="USWAN"){
 D3_filtered <- rbind(pregnancy_D3[imputed_end_of_pregnancy == 0][PROMPT == "yes"][!type_of_pregnancy_end %in% c("T", "SA")],
                      pregnancy_D3[imputed_end_of_pregnancy == 0][EUROCAT == "yes"])
 
@@ -20,7 +20,7 @@ pregnancy_D3<-D3_filtered
 rm(D3_filtered)
 }
 # FISABIO: PROMPT only quality green and yellow
-if(data_source_name=="FISABIO"){
+if(data_access_provider_name=="FISABIO"){
   
   # FISABIO: only birth registry
   list_of_meanings_FISABIO <- c("perinatal_death_registry_mother",
@@ -38,7 +38,7 @@ rm(D3_filtered)
 }
 
 # THL DP5: only pregnancies from registry 
-if(data_source_name=="NIHW"){
+if(data_access_provider_name=="NIHW"){
 list_of_meanings_THL = c("birth_registry",
                          "induced_termination_registry",
                          "birth_registry_child")
