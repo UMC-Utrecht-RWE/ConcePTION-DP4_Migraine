7abcfaa536be269cd7fe563cbb2f2bf5c3feb92a
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2024-03-07 10:54:53 +0100

    Update Step_00_a_D3_pregnancy_final_filter.R

--- p_steps/Step_00_a_D3_pregnancy_final_filter.R
+++ p_steps/Step_00_a_D3_pregnancy_final_filter.R
@@ -2,8 +2,8 @@ library(data.table)
 
 # UOSL: green and yellow from PROMPT and green yellow and blue from CONCEPTSET
 if(data_access_provider_name=="UOSL"){
-D3_filtered <- rbind(pregnancy_D3[(meaning_of_principal_record == "birth_registry_mother" & imputed_end_of_pregnancy == 0)],
-                     pregnancy_D3[!meaning_of_principal_record %in% c("birth_registry_mother", "PERSON_RELATIONSHIP")][highest_quality=="2_yellow"][type_of_pregnancy_end == "SA" | type_of_pregnancy_end == "T"])
+D3_filtered <- rbind(pregnancy_D3[meaning_of_principal_record == "birth_registry_mother" & imputed_end_of_pregnancy == 0], #prompts
+                     pregnancy_D3[!meaning_of_principal_record %in% c("birth_registry_mother", "PERSON_RELATIONSHIP")][highest_quality=="2_yellow"][type_of_pregnancy_end == "SA" | type_of_pregnancy_end == "T"]) #conceptsets
 rm(pregnancy_D3)
 pregnancy_D3<-D3_filtered
 rm(D3_filtered)
@@ -11,9 +11,10 @@ rm(D3_filtered)
 
 
 # SAIL: PROMPT & EUROCAT only quality green and yellow
+#FIX MEANINGS FROM THE PROMPTS, EUROCAT
 if(data_access_provider_name=="USWAN"){
-D3_filtered <- rbind(pregnancy_D3[imputed_end_of_pregnancy == 0][PROMPT == "yes"][!type_of_pregnancy_end %in% c("T", "SA")],
-                     pregnancy_D3[imputed_end_of_pregnancy == 0][EUROCAT == "yes"])
+D3_filtered <- rbind(pregnancy_D3[meaning_of_principal_record %in% c("birth_registry_mother")][imputed_end_of_pregnancy == 0][type_of_end_pregnancy %in% c("LB","SB")], #prompts
+                     pregnancy_D3[!meaning_of_principal_record %in% c("birth_registry_mother", "PERSON_RELATIONSHIP")][imputed_end_of_pregnancy == 0][type_of_pregnancy_end %in% c("SA", "T", "LB", "SB")]) #eurocat
 
 rm(pregnancy_D3)
 pregnancy_D3<-D3_filtered
@@ -23,27 +24,18 @@ rm(D3_filtered)
 if(data_access_provider_name=="FISABIO"){
   
   # FISABIO: only birth registry
-  list_of_meanings_FISABIO <- c("perinatal_death_registry_mother",
-                                "birth_registry_mother",
-                                "anomalies_mother_registry",   
-                                "perinatal_death_registry_child",
-                                "birth_registry_child",
-                                "anomalies_baby_registry")
-  
-D3_filtered <- pregnancy_D3[PROMPT == "yes" & imputed_end_of_pregnancy == 0][meaning_of_principal_record %in% list_of_meanings]
+  D3_filtered <-pregnancy_D3[meaning_of_principal_record %in% c("birth_registry_mother", "perinatal_death_registry_mother", "anomalies_mother_registry","perinatal_death_registry_child", "birth_registry_child")][imputed_end_of_pregnancy == 0][type_of_end_pregnancy=="LB" | type_of_end_pregnancy=="SB"] #prompts
 
-rm(pregnancy_D3)
+  rm(pregnancy_D3)
 pregnancy_D3<-D3_filtered
 rm(D3_filtered)
 }
 
 # THL DP5: only pregnancies from registry 
 if(data_access_provider_name=="NIHW"){
-list_of_meanings_THL = c("birth_registry",
-                         "induced_termination_registry",
-                         "birth_registry_child")
 
-D3_filtered <- pregnancy_D3[meaning_of_principal_record %in% list_of_meanings & imputed_end_of_pregnancy == 0]
+D3_filtered <- rbind(pregnancy_D3[meaning_of_principal_record %in% c("birth_registry","birth_registry_child") & imputed_end_of_pregnancy == 0][type_of_pregnancy_end == "LB" | type_of_pregnancy_end == "SB"],
+                     pregnancy_D3[meaning_of_principal_record %in% c("induced_termination_registry") & imputed_end_of_pregnancy == 0][type_of_pregnancy_end == "T"])
 
 rm(pregnancy_D3)
 pregnancy_D3<-D3_filtered
@@ -51,5 +43,26 @@ rm(D3_filtered)
 }
 
 
-# FERRARA DP5: only pregnancies from registry #needs clarifications
+# FERRARA DP4: only pregnancies from registry
+#FIX THE MEANINGS FOR CONCEPTSETS
+if(data_access_provider_name=="FERR"){
+  
+  D3_filtered <- rbind(pregnancy_D3[meaning_of_principal_record %in% c("birth_registry_mother","birth_registry_child")][imputed_start_of_pregnancy==0 & imputed_end_of_pregnancy == 0][type_of_pregnancy_end == "LB"], #prompts
+                       pregnancy_D3[!meaning_of_principal_record %in% c("birth_registry_mother","birth_registry_child")][highest_quality=="2_yellow"][type_of_pregnancy_end %in% c("LB", "SB", "SA", "T", "UNF")], #conceptsets
+                       pregnancy_D3[!meaning_of_principal_record %in% c("birth_registry_mother","birth_registry_child")][highest_quality=="4_red"][type_of_pregnancy_end %in% c("LOSTFU", "ONGOING", "UNK")]) #conceptsets
 
+  
+  rm(pregnancy_D3)
+  pregnancy_D3<-D3_filtered
+  rm(D3_filtered)
+}
+
+# EFEMERIS DP4
+if(data_access_provider_name=="CHUT"){
+  
+  D3_filtered <- pregnancy_D3[imputed_start_of_pregnancy==0 & imputed_end_of_pregnancy == 0][type_of_pregnancy_end %in% c("LB", "SB", "SA", "T", "UNK")] #prompts
+  
+  rm(pregnancy_D3)
+  pregnancy_D3<-D3_filtered
+  rm(D3_filtered)
+}
