aaa79340656fa2a90835c6ad5014a4e4742e52a5
Constanza <117450641+ConstanzaUtrecht@users.noreply.github.com>
2024-10-23 12:17:41 +0200

    Update Step_03_medicines_clean_up.R

--- p_steps/Step_03_medicines_clean_up.R
+++ p_steps/Step_03_medicines_clean_up.R
@@ -212,7 +212,7 @@ if(sum(length(med_gdm_medicines),length(med_migraine_medicines))>0){
 load(paste0(projectFolder,"/p_steps/Pregnancy algorithm/g_output/D3_mother_child_ids.RData"))
 
 #remove unecessary columns
-link<-as.data.table(D3_mother_child_id[c("person_id","child_id")])
+link<-as.data.table(D3_mother_child_ids[c("person_id","child_id")])
 
 #rename link[person_id] (mom_id) 
 setnames(link, "person_id", "mom_id") 
